0636b5900a7ff328575c8f4462c9fc22a3f9bd42
Jaypeeaaa <julianperges26@gmail.com>
2025-06-12 00:11:10 +0800

    Na connect yung index to homepage

--- Student  DIsciplinary Record/SplashScreen.js
+++ Student  DIsciplinary Record/SplashScreen.js
@@ -1,11 +1,10 @@
 document.querySelector("#loginForm").addEventListener("submit", function (e) {
   e.preventDefault();
+  
   const splash = document.getElementById("splashScreen");
   splash.classList.add("show");
 
   setTimeout(() => {
     window.location.href = "homepage.html";
-; // Redirects to homepage.html
-  }, 2000); // Delay before redirection
+  }, 2000); // 2 second splash screen delay
 });
-
