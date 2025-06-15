bfb7fe116c82b7932a855a38f1b9821465752729
Jedd <dejjcorona69@gmail.com>
2025-06-15 22:56:53 +0800

    major function update (debug nalang sa dashboard)

--- Student  DIsciplinary Record/SplashScreen.js
+++ Student  DIsciplinary Record/SplashScreen.js
@@ -1,9 +1,12 @@
+
+
+
 document.querySelector("#loginForm").addEventListener("submit", function (e) {
   e.preventDefault();
-  
+ 
   const splash = document.getElementById("splashScreen");
   splash.classList.add("show");
-
+ 
   setTimeout(() => {
     window.location.href = "homepage.html";
   }, 2000); // 2 second splash screen delay
